9fd76b89eb28e90165f7e7aae5a739fcf0454b4f
manish591 <manishdevrani777@gmail.com>
2021-08-26 11:41:32 +0530

    style-added hover to button

--- app.js
+++ app.js
@@ -6,3 +6,36 @@ const deleteLastItem = document.querySelector('.deletelastentry');
 const resetButton = document.querySelector('.reset');
 const equalButton = document.querySelector('.equalbutton');
 
+
+let currentNumberValue = '';
+let previousNumberValue;
+
+function updateNumberInView(e) {
+    currentNumberValue += `${e.target.innerText}`;
+    currentInputField.innerText = currentNumberValue;
+}
+
+numbersButton.forEach(button => {
+    button.addEventListener('click', updateNumberInView);
+})
+
+operatorButton.forEach(operator => {
+    operator.addEventListener('click', () => {
+        previousNumberValue = currentNumberValue;
+        currentNumberValue = '';
+        previousInputField.innerText = previousNumberValue;
+        currentInputField.innerText = currentNumberValue;
+    })
+})
+
+function calculate() {
+
+}
+
+function allClear() {
+
+}
+
+function deleteLastNumber() {
+
+}
