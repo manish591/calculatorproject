9072db3755c1d3fe934e2c7a68ed1124836586fe
manish591 <manishdevrani777@gmail.com>
2021-08-26 11:12:13 +0530

    added js file

---
+++ app.js
@@ -0,0 +1,8 @@
+const previousInputField = document.querySelector('.previousinputfield');
+const currentInputField = document.querySelector('.currentinputfield');
+const numbersButton = document.querySelectorAll('.number');
+const operatorButton = document.querySelectorAll('.operator');
+const deleteLastItem = document.querySelector('.deletelastentry');
+const resetButton = document.querySelector('.reset');
+const equalButton = document.querySelector('.equalbutton');
+
